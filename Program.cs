dafbb88740da3f462950ce8e8e98cd65a77c25a9
Justin Ridings <justin.blake.ridings@outlook.com>
2025-05-29 09:01:43 -0700

    Initial implementation

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AI.Foundry.Local;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using System.ClientModel;
+
+namespace FoundryConsole;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Foundry Local Initializations
+        var modelAlias = "phi-4";
+        // Note: If the model isn't already on-box, it will download and may take a few minutes for this step.
+        var manager = await FoundryLocalManager.StartModelAsync(aliasOrModelId: modelAlias);
+        var model = await manager.GetModelInfoAsync(aliasorModelId: modelAlias);
+
+        // "Negative Space" programming
+        if(manager == null || model == null)
+        {
+            throw new ArgumentNullException("Trouble initializing model");
+        }
+
+        var localEndpoint = manager.Endpoint; // http://localhost:NNNNN
+        var localApiKey = manager.ApiKey; // "OPEN AI APIKEY"
+
+        // Semantic Kernel Initializations
+        var builder = Kernel.CreateBuilder();
+
+        // Add OpenAI service
+        builder.Services.AddOpenAIChatCompletion(
+            modelId: model.ModelId,
+            endpoint: localEndpoint,
+            apiKey: localApiKey
+        );
+
+        var kernel = builder.Build();
+
+        var result = kernel.InvokePromptStreamingAsync("Tell me a joke about AI.");
+
+        // Stream the result.
+        await foreach (var chunk in result)
+        {
+            Console.Write(chunk);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # FoundryKernelConsole
-A simple C# console application that leverages the Foundry Local SDK &amp; Semantic Kernel in order to interact with LLM's on your local machine.
+#### A simple C# console application that leverages the Foundry Local SDK &amp; Semantic Kernel in order to interact with LLM's on your local machine.
+
+Reference: https://github.com/microsoft/Foundry-Local/blob/75c7126a/sdk/cs/README.md#L6-L29
+
+As of today, there is no public NuGet package available for the Foundry Local SDK. Once one is added, you can skip to Step 4.
+
+Step 1) Clone the Foundry Local repo:
+
+```bash
+git clone https://github.com/microsoft/Foundry-Local.git
+```
+
+Step 2) Build the Foundry Local SDK to generate a local NuGet:
+
+```bash
+cd Foundry-Local\sdk\cs
+dotnet build
+```
+Step 3) Add the local NuGet location as a Package Source in Visual Studio
+
+Step 4) Install the Foundry Local SDK (locally built) NuGet package in your project:
+
+```bash
+dotnet add package Microsoft.AI.Foundry.Local
+```
+
